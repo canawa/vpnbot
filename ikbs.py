7be2ce838361ae83e13579940f4e6672e1c78fe5
den <faceitlolaw@gmail.com>
2026-04-18 07:28:09 +0300

    zapusk

--- ikbs.py
+++ ikbs.py
@@ -24,7 +24,10 @@ def generate_ikb_main(user_id):
                 ikb_main.inline_keyboard.append([InlineKeyboardButton(text='Подключить VPN', callback_data='buy_vpn', icon_custom_emoji_id=get_emoji('plus'))])
                 if had_trial != 1:
                     ikb_main.inline_keyboard.append([InlineKeyboardButton(text='🎁 Попробовать бесплатно', callback_data='trial', style = 'success')])
-            
+        else:
+            ikb_main.inline_keyboard.append([InlineKeyboardButton(text='Подключить VPN', callback_data='buy_vpn', icon_custom_emoji_id=get_emoji('plus'))])
+            if had_trial != 1:
+                ikb_main.inline_keyboard.append([InlineKeyboardButton(text='🎁 Попробовать бесплатно', callback_data='trial', style = 'success')])
 
     ikb_main.inline_keyboard.append([
         InlineKeyboardButton(text='Реферальная программа', callback_data='referral', icon_custom_emoji_id=get_emoji('add_user')),
