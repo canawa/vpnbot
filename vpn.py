91218d1f9088ee44569342e41e840d090bc3b251
den <faceitlolaw@gmail.com>
2026-04-20 11:38:40 +0300

    possible fix

--- vpn.py
+++ vpn.py
@@ -2,6 +2,7 @@ import os
 import dotenv
 import asyncio
 import requests
+import sqlite3 as sq
 from datetime import datetime, time, timedelta
 import uuid
 import secrets
@@ -43,16 +44,21 @@ class Vpn:
         return body.json()
 
     def renew_subscription(self, tg_id, days):
+        with sq.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute('SELECT subscription_expires_at FROM subscriptions WHERE user_id = ?', (tg_id,))
+            expire_at = cur.fetchone()[0]
+            expire_at = datetime.fromisoformat(expire_at)
+            expire_at = max(expire_at, datetime.now())
         body = requests.patch(f"{self.base_url}/api/users",
            headers={
                "Content-Type": "application/json",
                "Authorization": f"Bearer {self.token}"
            },
            json={
-               "username": f'user_{tg_id}',
+                        "username": f'user_{tg_id}',
                           "trafficLimitBytes": 0,
-                          "expireAt": (datetime.now() + timedelta(days=days)).isoformat(),
-                          "createdAt": datetime.now().isoformat(),
+                          "expireAt": (expire_at + timedelta(days=days)).isoformat(),
                           "telegramId": tg_id,
                           "hwidDeviceLimit": 3,
                           "activeInternalSquads": ["6f11955f-6b95-4f96-bba4-3d866de8ce83"],
