56ebd745ecf9c843182d0f9337a9abe94341c7c2
den <faceitlolaw@gmail.com>
2026-04-20 12:19:45 +0300

    udpat

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ async def start_command(message):
 
     today_str = date.today().isoformat()
     user = vpn.get_user_by_tg_id(message.from_user.id)
-    expire_at_str = user.get('response', {}).get('expireAt')
+    expire_at_str = user['response']['expireAt']
 
     if expire_at_str:
         expire_at = datetime.fromisoformat(expire_at_str.replace('Z', '+00:00'))
